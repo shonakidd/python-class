3066900a3aebf7face4628e39bcbda2ebd08f8c0
shonakidd <shonakidd@Shonas-MacBook-Pro.local>
2019-10-31 19:58:01 -0400

    open 3 pictures

--- shona-slideshow.py
+++ shona-slideshow.py
@@ -1,3 +1,17 @@
 from PIL import Image
 image = Image.open("Photos/morgan1.jpg")
 image.show()
+
+import time
+ 
+# Wait for 2 seconds
+time.sleep(2)
+
+image = Image.open("Photos/morgan2.jpg")
+image.show()
+
+# Wait for 2 seconds
+time.sleep(2)
+
+image = Image.open("Photos/nyancat.jpeg")
+image.show()
